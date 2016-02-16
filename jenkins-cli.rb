ca88748d59dbee121661cc3dc768cd8990075063
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-02-16 10:59:51 -0800

    Make jenkins-cli work.

--- jenkins-cli.rb
+++ jenkins-cli.rb
@@ -1,14 +1,22 @@
 class JenkinsCli < Formula
-  desc "Install specified Jenkins CLI"
-  homepage "https://github.com/jacob-meacham/homebrew-formulae"
-  url "git@github.com:jacob-meacham/BLELib.git", :using => :git, :tag => "1.2.3"
-  head "git@github.com:DopplerLabs/BLELib.git", :using => :git
+  desc "Install Jenkins CLI"
+  homepage "https://jenkins-ci.org"
+  url "http://mirrors.jenkins-ci.org/war/1.647/jenkins.war"
+  sha256 "52778609d34cb532c934509bb2a63c77986ae8e9b7e85186bad0235f58e4200f"
+
+  bottle :unneeded
 
   depends_on :java => "1.7+"
 
   def install
+    system "jar", "xvf", "jenkins.war"
+
     libexec.install Dir["**/jenkins-cli.jar"]
     bin.write_jar_script libexec/"jenkins-cli.jar", "jenkins-cli"
+
+    if not ENV['JENKINS_URL'].is_set?
+      ohai 'JENKINS_URL environment variable is not set. Consider setting it to your most commonly used Jenkins instance.'
+    end
 
   test do
     system "jenkins-cli", "--help"
