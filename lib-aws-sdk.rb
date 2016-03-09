d1ac80c345d116be2f1ebc952aa3ef055b58a4ef
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-03-09 11:05:06 -0800

    Add support for all clients.

--- lib-aws-sdk.rb
+++ lib-aws-sdk.rb
@@ -1,12 +1,20 @@
 class LibAwsSdk < Formula
   url "https://github.com/awslabs/aws-sdk-cpp/archive/0.9.6.tar.gz"
-  sha256 "2d26995974eb03b6ba6d26c32f9956e95d5a3d0db6b5fa149a2df7e4773112f4"
+  sha256 "cf2dad62e50c5c5cff88a4ad959af3aa402aa7e8aa1692bdf807052c0fe4aeb9"
   homepage "http://aws.amazon.com/"
 
-  option "with-s3",
-         "Build s3 client"
-  option "with-ec2",
-         "Build ec2 client"
+  @@allClients = ['access-management', 'acm', 'apigateway', 'autoscaling', 'cloudformation',
+    'cloudfront', 'cloudhsm', 'cloudsearch', 'cloudtrail', 'codecommit', 'codedeploy', 'codepipeline',
+    'cognito-identity', 'cognito-sync', 'config', 'datapipeline', 'devicefarm', 'directconnect',
+    'ds', 'dynamobdb', 'ec2', 'ecr', 'ecs', 'elasticache', 'elasticbeanstalk', 'elasticfilesystem',
+    'elasticloadbalancing', 'elasticmapreduce', 'elastictranscoder', 'email', 'es', 'events', 'firehose',
+    'gamelift', 'glacier', 'iam', 'identity-management', 'importexport', 'inspector', 'iot', 'kinesis',
+    'kms', 'lambda', 'logs', 'machinelearning', 'marketplacecommerceanalytics', 'mobileanalytics',
+    'monitoring', 'opsworks', 'queues', 'rds', 'redshift', 'route53', 's3', 'sdb', 'sns', 'sqs',
+    'storagegateway', 'sts', 'support', 'swf', 'transfer', 'waf', 'workspaces']
+
+  @@allClients.each { |client|  option "with-#{client}", "Build with #{client} client"}
+
   option "with-custom-memory-management",
          "Build with custom memory management"
 
@@ -14,23 +22,28 @@ class LibAwsSdk < Formula
 
   def install
     clients = []
-    clients << 'aws-cpp-sdk-s3' if build.with? "s3"
-    clients << 'aws-cpp-sdk-ec2' if build.with? "ec2"
+    @@allClients.each { |client|
+      clients << "aws-cpp-sdk-#{client}" if build.with? client
+    }
+
+    custom_memory_management = (build.with? "custom-memory-management") ? 1 : 0
 
     args = std_cmake_args
-    args << '-DCUSTOM_MEMORY_MANAGEMENT=' + build.with? "custom-memory-management" 1 else 0
+    args << "-DCUSTOM_MEMORY_MANAGEMENT=#{custom_memory_management}"
     if clients.any?
-      args << 'BUILD_ONLY=' + clients.join(';')
+      args << "-DBUILD_ONLY=#{clients.join(';')}"
     end
 
     mkdir "build" do
-      system "cmake", "..", *args
+      system "cmake", *args, ".."
       system "make"
       system "make", "install"
     end
 
+    mv lib/"mac/Release", lib/"aws"
+  end
+
   test do
     raise
   end
-
 end
