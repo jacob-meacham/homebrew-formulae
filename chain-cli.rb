29be04be5571ca7e52767a48a21def94f331682b
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-08-29 13:14:50 -0700

    Update to new recommendations for python patckages

--- chain-cli.rb
+++ chain-cli.rb
@@ -4,26 +4,17 @@ class ChainCli < Formula
   url "git@github.com:jacob-meacham/chain-cli.git", :using => :git, :tag => "0.1.0"
   head "git@github.com:jacob-meacham/chain-cli.git", :using => :git, :branch => "develop"
 
-  depends_on :python
+  depends_on :python3
 
   resource "click" do
     url "https://pypi.python.org/packages/source/c/click/click-6.6.tar.gz"
     sha256 "cc6a19da8ebff6e7074f731447ef7e112bd23adf3de5c597cf9989f2fd8defe9"
   end
 
+  include Language::Python::Virtualenv
+
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-    %w[click].each do |r|
-      resource(r).stage do
-        system "python", *Language::Python.setup_install_args(libexec/"vendor")
-      end
-    end
-
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    system "python", *Language::Python.setup_install_args(libexec)
-
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    virtualenv_install_with_resources
 
     bash_completion.install "contrib/bash/chain-complete.bash" => "chain"
   end
